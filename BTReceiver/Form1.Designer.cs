0b902ba98d4568f7ab3afe5208c06d8a59b46684
roman zinchuk <zigzagforever@mail.ru>
2016-09-27 01:16:24 +0300

    cannot connect now, but added multithreading

--- BTReceiver/Form1.Designer.cs
+++ BTReceiver/Form1.Designer.cs
@@ -28,40 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.rtbOutData = new System.Windows.Forms.RichTextBox();
             this.btnConnect = new System.Windows.Forms.Button();
             this.devicesList = new System.Windows.Forms.ListBox();
-            this.btnUpdate = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.lbInternalBT = new System.Windows.Forms.ListBox();
             this.btnUpdateInternalBtAdapters = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.btnReadFromBt = new System.Windows.Forms.Button();
             this.btnWriteToBt = new System.Windows.Forms.Button();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.gbUpdateDeviceListOperationType = new System.Windows.Forms.GroupBox();
+            this.rbUpdateFromMemory = new System.Windows.Forms.RadioButton();
+            this.rbUpdateDefault = new System.Windows.Forms.RadioButton();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblBtOperationStatus = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.gbUpdateDeviceListOperationType.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbOutData
             // 
             this.rtbOutData.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtbOutData.Location = new System.Drawing.Point(140, 33);
+            this.rtbOutData.Location = new System.Drawing.Point(140, 189);
             this.rtbOutData.Name = "rtbOutData";
-            this.rtbOutData.Size = new System.Drawing.Size(723, 164);
+            this.rtbOutData.Size = new System.Drawing.Size(723, 108);
             this.rtbOutData.TabIndex = 0;
             this.rtbOutData.Text = "";
             // 
             // btnConnect
             // 
-            this.btnConnect.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnConnect.Location = new System.Drawing.Point(140, 3);
+            this.btnConnect.Location = new System.Drawing.Point(3, 107);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(723, 24);
+            this.btnConnect.Size = new System.Drawing.Size(128, 24);
             this.btnConnect.TabIndex = 1;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -71,43 +72,30 @@
             // 
             this.devicesList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.devicesList.FormattingEnabled = true;
-            this.devicesList.Location = new System.Drawing.Point(3, 33);
+            this.devicesList.Location = new System.Drawing.Point(3, 189);
             this.devicesList.Name = "devicesList";
-            this.devicesList.Size = new System.Drawing.Size(131, 164);
+            this.devicesList.Size = new System.Drawing.Size(131, 108);
             this.devicesList.TabIndex = 2;
-            // 
-            // btnUpdate
-            // 
-            this.btnUpdate.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnUpdate.Location = new System.Drawing.Point(3, 3);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(131, 24);
-            this.btnUpdate.TabIndex = 3;
-            this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseVisualStyleBackColor = true;
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.92593F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.07407F));
-            this.tableLayoutPanel1.Controls.Add(this.btnUpdate, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.rtbOutData, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btnConnect, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.devicesList, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.lbInternalBT, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.btnUpdateInternalBtAdapters, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.chart1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.12605F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.87395F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 68F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 126F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 62F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 58F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(866, 395);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
@@ -115,17 +103,17 @@
             // 
             this.lbInternalBT.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lbInternalBT.FormattingEnabled = true;
-            this.lbInternalBT.Location = new System.Drawing.Point(3, 271);
+            this.lbInternalBT.Location = new System.Drawing.Point(3, 340);
             this.lbInternalBT.Name = "lbInternalBT";
-            this.lbInternalBT.Size = new System.Drawing.Size(131, 121);
+            this.lbInternalBT.Size = new System.Drawing.Size(131, 52);
             this.lbInternalBT.TabIndex = 4;
             // 
             // btnUpdateInternalBtAdapters
             // 
             this.btnUpdateInternalBtAdapters.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnUpdateInternalBtAdapters.Location = new System.Drawing.Point(3, 203);
+            this.btnUpdateInternalBtAdapters.Location = new System.Drawing.Point(3, 303);
             this.btnUpdateInternalBtAdapters.Name = "btnUpdateInternalBtAdapters";
-            this.btnUpdateInternalBtAdapters.Size = new System.Drawing.Size(131, 62);
+            this.btnUpdateInternalBtAdapters.Size = new System.Drawing.Size(131, 31);
             this.btnUpdateInternalBtAdapters.TabIndex = 5;
             this.btnUpdateInternalBtAdapters.Text = "Update radiomodules";
             this.btnUpdateInternalBtAdapters.UseVisualStyleBackColor = true;
@@ -136,9 +124,9 @@
             this.flowLayoutPanel1.Controls.Add(this.btnReadFromBt);
             this.flowLayoutPanel1.Controls.Add(this.btnWriteToBt);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(140, 203);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(140, 303);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(723, 62);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(723, 31);
             this.flowLayoutPanel1.TabIndex = 6;
             // 
             // btnReadFromBt
@@ -161,22 +149,68 @@
             this.btnWriteToBt.UseVisualStyleBackColor = true;
             this.btnWriteToBt.Click += new System.EventHandler(this.btnWriteToBt_Click);
             // 
-            // chart1
+            // flowLayoutPanel2
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(140, 271);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(723, 121);
-            this.chart1.TabIndex = 7;
-            this.chart1.Text = "chart1";
+            this.flowLayoutPanel2.Controls.Add(this.gbUpdateDeviceListOperationType);
+            this.flowLayoutPanel2.Controls.Add(this.btnUpdate);
+            this.flowLayoutPanel2.Controls.Add(this.btnConnect);
+            this.flowLayoutPanel2.Controls.Add(this.lblBtOperationStatus);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(131, 180);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // gbUpdateDeviceListOperationType
+            // 
+            this.gbUpdateDeviceListOperationType.Controls.Add(this.rbUpdateFromMemory);
+            this.gbUpdateDeviceListOperationType.Controls.Add(this.rbUpdateDefault);
+            this.gbUpdateDeviceListOperationType.Location = new System.Drawing.Point(3, 3);
+            this.gbUpdateDeviceListOperationType.Name = "gbUpdateDeviceListOperationType";
+            this.gbUpdateDeviceListOperationType.Size = new System.Drawing.Size(128, 70);
+            this.gbUpdateDeviceListOperationType.TabIndex = 5;
+            this.gbUpdateDeviceListOperationType.TabStop = false;
+            this.gbUpdateDeviceListOperationType.Text = "Тип пошуку";
+            // 
+            // rbUpdateFromMemory
+            // 
+            this.rbUpdateFromMemory.AutoSize = true;
+            this.rbUpdateFromMemory.Checked = true;
+            this.rbUpdateFromMemory.Location = new System.Drawing.Point(7, 44);
+            this.rbUpdateFromMemory.Name = "rbUpdateFromMemory";
+            this.rbUpdateFromMemory.Size = new System.Drawing.Size(95, 17);
+            this.rbUpdateFromMemory.TabIndex = 1;
+            this.rbUpdateFromMemory.TabStop = true;
+            this.rbUpdateFromMemory.Text = "Запамятовані";
+            this.rbUpdateFromMemory.UseVisualStyleBackColor = true;
+            // 
+            // rbUpdateDefault
+            // 
+            this.rbUpdateDefault.AutoSize = true;
+            this.rbUpdateDefault.Location = new System.Drawing.Point(7, 20);
+            this.rbUpdateDefault.Name = "rbUpdateDefault";
+            this.rbUpdateDefault.Size = new System.Drawing.Size(79, 17);
+            this.rbUpdateDefault.TabIndex = 0;
+            this.rbUpdateDefault.Text = "Звичайний";
+            this.rbUpdateDefault.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(3, 79);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(128, 22);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblBtOperationStatus
+            // 
+            this.lblBtOperationStatus.AutoSize = true;
+            this.lblBtOperationStatus.Location = new System.Drawing.Point(3, 134);
+            this.lblBtOperationStatus.Name = "lblBtOperationStatus";
+            this.lblBtOperationStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblBtOperationStatus.TabIndex = 6;
             // 
             // Form1
             // 
@@ -188,7 +222,10 @@
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.gbUpdateDeviceListOperationType.ResumeLayout(false);
+            this.gbUpdateDeviceListOperationType.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -198,14 +235,18 @@
         private System.Windows.Forms.RichTextBox rtbOutData;
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.ListBox devicesList;
-        private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.ListBox lbInternalBT;
         private System.Windows.Forms.Button btnUpdateInternalBtAdapters;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button btnReadFromBt;
         private System.Windows.Forms.Button btnWriteToBt;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.GroupBox gbUpdateDeviceListOperationType;
+        private System.Windows.Forms.RadioButton rbUpdateFromMemory;
+        private System.Windows.Forms.RadioButton rbUpdateDefault;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblBtOperationStatus;
     }
 }
 
--- BTReceiver/Form1.cs
+++ BTReceiver/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Sockets;
+using System.Threading;
 
 namespace BTReceiver
 {
@@ -23,6 +24,7 @@ namespace BTReceiver
             InitializeComponent();
             updateBtAdapters(false);
             initBluetooth();
+            disableConnectFunction();
         }
         private void initStartingUI()
         {
@@ -61,7 +63,8 @@ namespace BTReceiver
         {
             try
             {
-                bluetoothClient = new BluetoothClient();
+                if (bluetoothClient!=null) bluetoothClient.Close();
+                 bluetoothClient = new BluetoothClient();
             }
             catch (PlatformNotSupportedException e)
             {
@@ -101,10 +104,12 @@ namespace BTReceiver
                 Console.WriteLine("Cannot connect to null");
                 return;
             }
-           // bluetoothClient.SetPin("1234");
+            bluetoothClient.SetPin("1234");
+           // BluetoothSecurity.PairRequest(device.DeviceAddress, "1234");
             bluetoothClient.BeginConnect(device.DeviceAddress, BluetoothService.RFCommProtocol, new AsyncCallback(DeviceConnectedCallback),
                 device);
-            Console.WriteLine("Connected:"+selectedDevice);
+            disableConnectFunction();
+ 
         }
 
         private void DeviceConnectedCallback(IAsyncResult result)
@@ -114,6 +119,7 @@ namespace BTReceiver
                    //
 
                 this.BeginInvoke(new MethodInvoker(delegate { enableWritingToBtUI(); }));
+                Console.WriteLine("Connected");
                 //
             }
         }
@@ -129,33 +135,49 @@ namespace BTReceiver
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            var devices = bluetoothClient.DiscoverDevices();
-            Console.WriteLine("Bluetooth devices:");
-            devicesList.Items.Clear();
-            foreach (var device in devices)
-            {
-                var blueToothInfo =
-                    string.Format(
-                        "- DeviceName: {0}{1}  Connected: {2}{1}  Address: {3}{1}  Last seen: {4}{1}  Last used: {5}{1}",
-                        device.DeviceName, Environment.NewLine, device.Connected, device.DeviceAddress, device.LastSeen,
-                        device.LastUsed);
-                blueToothInfo += string.Format("  Class of device{0}   Device: {1}{0}   Major Device: {2}{0}   Service: {3}",
-                Environment.NewLine, device.ClassOfDevice.Device, device.ClassOfDevice.MajorDevice,
-                 device.ClassOfDevice.Service);
-                Console.WriteLine(blueToothInfo);
-                Console.WriteLine(); 
-                devicesList.Items.Add(new BluetoothDeviceInfoContainer(device));
-
-            }
-            if (devices.Length > 0)
-            {
-                enableConnectFunction();
-            }
+           lblBtOperationStatus.Text = "Триває оновлення пристроїв bluetooth";
+            if (rbUpdateDefault.Checked)
+               bluetoothClient.BeginDiscoverDevices(10, true, true, true, false, updateBTDeviceList, bluetoothClient);
             else
+            bluetoothClient.BeginDiscoverDevices(10, true, true,false,false, updateBTDeviceList, bluetoothClient);
+            //var devices = bluetoothClient.DiscoverDevices();
+        }
+        void updateBTDeviceList(IAsyncResult result)
+        {
+          //Get back the BluetoothClient which was passed as the state parameter in BeginDiscoverDevices
+            BluetoothClient thisDevice = result.AsyncState as BluetoothClient;
+        Console.WriteLine("Bluetooth devices:");
+            BluetoothDeviceInfo[] devices = thisDevice.EndDiscoverDevices(result);
+            this.BeginInvoke(new MethodInvoker(delegate
             {
-                disableConnectFunction();
-            }
+                enableWritingToBtUI();
+                devicesList.Items.Clear();
+                foreach (var device in devices)
+                {
+                    var blueToothInfo =
+                        string.Format(
+                            "- DeviceName: {0}{1}  Connected: {2}{1}  Address: {3}{1}  Last seen: {4}{1}  Last used: {5}{1}",
+                            device.DeviceName, Environment.NewLine, device.Connected, device.DeviceAddress, device.LastSeen,
+                            device.LastUsed);
+                    blueToothInfo += string.Format("  Class of device{0}   Device: {1}{0}   Major Device: {2}{0}   Service: {3}",
+                    Environment.NewLine, device.ClassOfDevice.Device, device.ClassOfDevice.MajorDevice,
+                     device.ClassOfDevice.Service);
+                    Console.WriteLine(blueToothInfo);
+                    Console.WriteLine();
+                    devicesList.Items.Add(new BluetoothDeviceInfoContainer(device));
 
+                }
+                if (devices.Length > 0)
+                {
+                    enableConnectFunction();
+                }
+                else
+                {
+                    disableConnectFunction();
+                }
+                lblBtOperationStatus.Text = "";
+            }));
+        
         }
 
         private void serializeDeviceListToFile()
@@ -179,55 +201,94 @@ namespace BTReceiver
             }
         }
 
-        private void btnAskDevice_Click(object sender, EventArgs e)
+        private void BeginReadDataFromBT()
         {
             const bool IS_FLOATING_POINT = false;
             const int CHANNEL_COUNT = 2;
             const int BIT_DEPTH = 16;
             const int SAMPLE_RATE = 22050;
-       
-
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            int totalBytesReaded = 0;
-            using (FileStream fsSource = new FileStream("test.wav",
-                FileMode.OpenOrCreate, FileAccess.Write))
+           
+            new Thread(delegate()
             {
-                ZigzagAudioLibrary.WriteWavHeader(fsSource, IS_FLOATING_POINT, CHANNEL_COUNT,
-                    BIT_DEPTH, SAMPLE_RATE, 0);
-
-                while (bluetoothClient.GetStream().DataAvailable)
+                int totalBytesReaded = 0;
+                bool dataAvailable = true;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                using (FileStream fsSource = new FileStream("test.wav",
+             FileMode.OpenOrCreate, FileAccess.Write))
                 {
-                    int readed = bluetoothClient.GetStream().Read(btReceiverBuffer, 0, btReceiverBuffer.Length);
-                    Console.WriteLine("received bytes:" + readed);
-                    Console.Write("bytes:");
-                /*    foreach (byte b in btReceiverBuffer)
+                    ZigzagAudioLibrary.WriteWavHeader(fsSource, IS_FLOATING_POINT, CHANNEL_COUNT,
+                        BIT_DEPTH, SAMPLE_RATE, 0);
+                   
+                    while (dataAvailable)
                     {
-                        Console.Write(b+" ");
-                    }*/
-                    Console.WriteLine();
-                    fsSource.Write(btReceiverBuffer,0,readed);
-                    totalBytesReaded += readed;
+                        try
+                        {
+                            dataAvailable = bluetoothClient.GetStream().DataAvailable;
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            this.BeginInvoke(new MethodInvoker(delegate
+                            {
+                                MessageBox.Show(e.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }));
+                            dataAvailable = false;
+                            return;
+                        }
+                        int readed = 0;
+                        try
+                        {
+                            readed = bluetoothClient.GetStream().Read(btReceiverBuffer, 0, btReceiverBuffer.Length);
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            this.BeginInvoke(new MethodInvoker(delegate
+                            {
+                                MessageBox.Show(e.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }));
+                            dataAvailable = false;
+                            return;
+                        }
+                     
+                        Console.WriteLine("received bytes:" + readed);
+                        Console.Write("bytes:");
+                        /*    foreach (byte b in btReceiverBuffer)
+                            {
+                                Console.Write(b+" ");
+                            }*/
+                        Console.WriteLine();
+                        fsSource.Write(btReceiverBuffer, 0, readed);
+                        totalBytesReaded += readed;
+                    }
+                    int totalSampleCount = totalBytesReaded / BIT_DEPTH;
+                    Console.WriteLine("totalSampleCount:" + totalSampleCount);
+                    //Записуєм розмір сирих аудіоданих
+                    ZigzagAudioLibrary.writeSubChunc2Size(fsSource, BIT_DEPTH, totalSampleCount);
+                    double elapsedMs = watch.Elapsed.TotalMilliseconds;
+                    Console.WriteLine("receiving time:" + elapsedMs);
+                    fsSource.Flush();
+                    fsSource.Close();
+                    watch.Stop();
                 }
-                int totalSampleCount = totalBytesReaded/BIT_DEPTH;
-                Console.WriteLine("totalSampleCount:"+ totalSampleCount);
-                //Записуєм розмір сирих аудіоданих
-                ZigzagAudioLibrary.writeSubChunc2Size(fsSource,BIT_DEPTH, totalSampleCount);
-                double elapsedMs = watch.Elapsed.TotalMilliseconds;
-                Console.WriteLine("receiving time:" + elapsedMs);
-                fsSource.Flush();
-               fsSource.Close();
-               
-            }
-            watch.Stop();
-          
+            }).Start();
+        }
 
+        private void btnAskDevice_Click(object sender, EventArgs e)
+        {
+            BeginReadDataFromBT();
         }
 
         private void btnWriteToBt_Click(object sender, EventArgs e)
         {
             byte[] arr = new byte[1];
             arr[0] = 53;
-            bluetoothClient.GetStream().Write(arr, 0, 1);
+            try
+            {
+                bluetoothClient.GetStream().Write(arr, 0, 1);
+            }
+            catch (InvalidOperationException exception)
+            {
+                MessageBox.Show(exception.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
